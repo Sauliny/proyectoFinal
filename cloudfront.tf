44960edc0cdaac04f1cb79fcfe17a97e1ddd01b1
SauloEspitia <sseg82@gmail.com>
2024-02-23 11:28:22 -0500

    Archivo Modificado: net6

--- cloudfront.tf
+++ cloudfront.tf
@@ -24,25 +24,20 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     origin_access_control_id = aws_cloudfront_origin_access_control.OAC_ProyFinal.id
     origin_id                = local.s3_origin_id
   }
-
   enabled             = true
   is_ipv6_enabled     = true
   comment             = "Some comment"
   default_root_object = "index.html"
-
   logging_config {
     include_cookies = false
     bucket          = "mylogs.s3.amazonaws.com"
     prefix          = "myprefix"
   }
-
   aliases = ["mysite.example.com", "yoursite.example.com"]
-
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
-
     forwarded_values {
       query_string = false
 
@@ -50,7 +45,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
         forward = "none"
       }
     }
-
     viewer_protocol_policy = "allow-all"
     min_ttl                = 0
     default_ttl            = 3600
