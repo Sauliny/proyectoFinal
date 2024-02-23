ce359d80b6c6f995771f9d844d8e31b9e6c1ad08
SauloEspitia <sseg82@gmail.com>
2024-02-23 15:53:03 -0500

    Archivo Modificado 3

--- ecs.tf
+++ ecs.tf
@@ -40,10 +40,12 @@ resource "aws_ecs_task_definition" "task-ecs-proyfinal" {
 
 ## Se configura el Servicio para ECS
 resource "aws_ecs_service" "srv-ecs-proyfinal" {
-  name            = "srv-ecs-proyfinal"
-  cluster         = aws_ecs_cluster.ecsProyFinal.id
-  task_definition = aws_ecs_task_definition.task-ecs-proyfinal.arn
-  desired_count   = 2   # cantidad de instancias para ejecutar
+  name                = "srv-ecs-proyfinal"
+  cluster             = aws_ecs_cluster.ecsProyFinal.id
+  task_definition     = aws_ecs_task_definition.task-ecs-proyfinal.arn
+  desired_count       = 2   # cantidad de instancias para ejecutar
+  iam_role            = aws_iam_role.ecs_task_execution_role.arn
+#  depends_on          = [aws_iam_policy.  ]   
 
   # Configuración del servicio para Fargate
   launch_type = "FARGATE"
@@ -57,7 +59,7 @@ resource "aws_ecs_service" "srv-ecs-proyfinal" {
   load_balancer {
     target_group_arn = aws_lb_target_group.lb-tg-proyfinal.arn
     container_name   = "conteinerProyFinal"
-    container_port   = 8080
+    container_port   = 80
   }
   /*
   lifecycle {
