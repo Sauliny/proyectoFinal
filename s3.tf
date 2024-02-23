68031b0f2a1bd756e53faf1a09865f99c68fca76
SauloEspitia <sseg82@gmail.com>
2024-02-23 16:16:05 -0500

    Archivo Modificado 4

--- s3.tf
+++ s3.tf
@@ -12,13 +12,22 @@ resource "aws_s3_bucket" "cf-s3-proyfinal" {
   }
 }
 
+## Se configura el aws_s3_bucket_policy para Cloudfront en AWS
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.s3_bucket_policy.json
+}
+
+
 ## Se configura el acl del bucket s3 para Cloudfront en AWS
-/*
+
 resource "aws_s3_bucket_acl" "cf-s3-acl-ProyFinal" {
+#  depends_on = [aws_s3_bucket_ownership_controls.bucket_controls_pf]
   bucket = aws_s3_bucket.cf-s3-proyfinal.id
   acl    = "private"
 }
-*/
+
 
 ## Se configura el origin para Cloudfront en AWS
 locals {
