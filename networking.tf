581084d927224d7741d065303cef62ecb4a7b288
SauloEspitia <sseg82@gmail.com>
2024-02-23 09:43:58 -0500

    Archivo Modificado: net

--- networking.tf
+++ networking.tf
@@ -97,7 +97,7 @@ resource "aws_lb_target_group" "lb-tg-ProyFinal" {
 resource "aws_lb_listener" "lstnProyFinal" {
   load_balancer_arn = aws_lb.lbProyFinal.arn
   port              = "80"
-  protocol          = "TCP"
+  protocol          = "HTTP"
 ##ssl_policy        = "ELBSecurityPolicy-2016-08"
 ##certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
   default_action {
