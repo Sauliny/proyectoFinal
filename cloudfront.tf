a68fceb2143cf24d2bb97192634048b186220bb9
SauloEspitia <sseg82@gmail.com>
2024-02-23 11:44:28 -0500

    Archivo Modificado: cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -113,3 +113,31 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     cloudfront_default_certificate = true
   }
 }
+
+## Se configura el aws_s3_bucket_policy ##
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = aws_s3_bucket.b.id
+
+  policy = jsonencode(
+    {
+        "Version": "2008-10-17",
+        "Id": "PolicyForCloudFrontPrivateContent",
+        "Statement": [
+            {
+                "Sid": "AllowCloudFrontServicePrincipal",
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "cloudfront.amazonaws.com"
+                },
+                "Action": "s3:GetObject",
+                "Resource": "arn:aws:s3:::vivi-web-component-dev/*",
+                "Condition": {
+                    "StringEquals": {
+                        "AWS:SourceArn": "arn:aws:cloudfront::437209357514:distribution/E21N71EARA5PM5"
+                    }
+                }
+            }
+        ]
+    }
+  )
+}
