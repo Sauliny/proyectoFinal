398c75c580208585dbd68107187434401dc9324a
Sauliny <159931749+Sauliny@users.noreply.github.com>
2024-02-26 11:28:30 -0500

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -38,6 +38,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 #  aliases = []
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+#    allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
     forwarded_values {
