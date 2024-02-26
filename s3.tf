dcadde6a3faab7fd2206e63e7a3643222ec4a9a4
Sauliny <159931749+Sauliny@users.noreply.github.com>
2024-02-26 13:17:52 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -49,6 +49,7 @@ resource "aws_s3_bucket_acl" "cf-s3-acl-proyfinal" {
 }
 resource "aws_s3_bucket_public_access_block" "public_access_block_aws_s3" {
   bucket = aws_s3_bucket.cf-s3-proyfinal.id
+  
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
